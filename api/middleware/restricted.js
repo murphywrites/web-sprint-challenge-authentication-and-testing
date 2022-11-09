b01ed0980bf21b796957b5e96d16d7c9000a7f58
Michael Murphy <mike@murphyscode.com>
2022-11-09 08:45:43 -0800

    wip

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,10 @@
 module.exports = (req, res, next) => {
-  next();
+  if (req.session.user) {
+    next();
+  } else {
+    res.status(401).json({message:'token required'})
+  }
+
   /*
     IMPLEMENT
 
